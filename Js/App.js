435cc3fbde05f1d8723f21e1650a5f214f7d6a03
EduardaBedetti <bedettieduarda29@gmail.com>
2025-07-08 10:55:13 -0300

    Add files via upload

---
+++ Js/App.js
@@ -0,0 +1,22 @@
+alert("Boas vindas ao nosso site");
+let nome = "Lua";
+let idade = 25;
+let NumeroDeVendas = 50;
+let saldoDisponivel = 1000;
+
+alert("Erro! preencha todos os campos");
+let mensagemDeErro = "Erro! preencha todos os campos";
+alert(mensagemDeErro)
+
+let Nome = prompt("Qual o seu nome?");
+let Idade = prompt("Qual a sua idade?");
+
+if (Idade >= 18) {
+    alert("Pode tirar habilitação")
+} else {
+    alert("Idade insulficiente!");
+}
+
+
+
+
